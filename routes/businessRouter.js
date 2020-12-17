634afc35dab4f61b389f36619c9c6ae64b1ae08c
Alberto <alpagor@gmail.com>
2020-12-17 12:21:17 +0100

    /business test refactor

--- routes/businessRouter.js
+++ routes/businessRouter.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const business = require("../models/business");
 const businessRouter = express.Router();
-const Business = require('../models/business')
+const Business = require("../models/business");
 
 // GET  "/business/" show all business
 businessRouter.get("/", async (req, res) => {
+  try {
     const businesses = await Business.find();
     res.send(businesses).status(200);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = businessRouter;
